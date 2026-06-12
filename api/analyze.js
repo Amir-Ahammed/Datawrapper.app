27fe42cdcf997e9437420f8d112f9ac4dd9dab8e
Amir Ahammed <amir.ahammed@selisegroup.com>
2026-06-12 17:16:40 +0600

    update serverless v0.3

--- api/analyze.js
+++ api/analyze.js
@@ -1,12 +1,20 @@
 const { handleUpload } = require("../server");
 
 module.exports = async function analyze(req, res) {
-  if (req.method !== "POST") {
-    res.setHeader("Allow", "POST");
-    res.statusCode = 405;
-    res.end("Method not allowed");
-    return;
-  }
+  try {
+    if (req.method !== "POST") {
+      res.setHeader("Allow", "POST");
+      res.statusCode = 405;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ error: "Method not allowed" }));
+      return;
+    }
 
-  await handleUpload(req, res, { persist: false });
+    await handleUpload(req, res, { persist: false });
+  } catch (error) {
+    console.error("Function error:", error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "Internal server error" }));
+  }
 };
